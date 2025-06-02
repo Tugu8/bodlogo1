799126e4750a1e50974004e9ed7093ec15ef9f16
Tugu8 <tugutugu018@gmail.com>
2025-06-02 13:46:21 +0800

    Initial commit

---
+++ bodlogo1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class Animal {
+    void walk() {
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
+
+public class bodlogo1 {
+    public static void main(String args[]) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
